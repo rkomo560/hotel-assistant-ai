9e88e188afd817e119197531c6caddcfd378c6d2
rkomo560 <abdelhafid22rmiki@gmail.com>
2025-06-11 17:09:05 +0100

    Ajout de Procfile et requirements.txt pour Railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from chatbot.model import AssistantHotel
 
@@ -20,3 +21,7 @@ def ask():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
